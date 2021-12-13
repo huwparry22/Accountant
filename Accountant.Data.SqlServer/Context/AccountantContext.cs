757c489538d37fa435791fb54aa2ad9899950601
Huw Parry <huwparry22@hotmail.com>
2021-12-13 17:27:42 +0000

    Added db context. Removed nullable warnings.

---
+++ Accountant.Data.SqlServer/Context/AccountantContext.cs
@@ -0,0 +1,24 @@
+﻿using Accountant.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accountant.Data.SqlServer.Context
+{
+    public class AccountantContext : DbContext
+    {
+        public AccountantContext(DbContextOptions<AccountantContext> dbContextOptions) : base(dbContextOptions) { }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Entry> Entrys { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
