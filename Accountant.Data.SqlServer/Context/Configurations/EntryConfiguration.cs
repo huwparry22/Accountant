5cd57dad77d9c378fb992b4535fddec2eadbcb31
Huw Parry <huwparry22@hotmail.com>
2021-12-13 17:33:29 +0000

    Added entry configuration.

---
+++ Accountant.Data.SqlServer/Context/Configurations/EntryConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Accountant.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Accountant.Data.SqlServer.Context.Configurations
+{
+    internal class EntryConfiguration : IEntityTypeConfiguration<Entry>
+    {
+        public void Configure(EntityTypeBuilder<Entry> builder)
+        {
+            builder.ToTable("Entry");
+
+            builder.HasKey(e => e.EntryId);
+            builder.Property(e => e.EntryId).UseIdentityColumn();
+        }
+    }
+}
