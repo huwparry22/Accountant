526df5d0287fcef61a30952ba039f05fbebc5a2f
Huw Parry <huwparry22@hotmail.com>
2021-12-13 17:19:59 +0000

    Added DI resolution class.

---
+++ Accountant.Data.SqlServer/DependencyResolution/ProjectServices.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accountant.Data.SqlServer.DependencyResolution
+{
+    public static class ProjectServices
+    {
+        public static void AddSqlServerServices(this IServiceCollection services)
+        {
+            
+        }
+    }
+}
