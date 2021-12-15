257dfc3a45b1fd834842727d3b6a412997e590f1
Huw Parry <huwparry22@hotmail.com>
2021-12-15 16:55:03 +0000

    Added configuration builder helper class.

---
+++ Accountant.SolutionBuilder/ConfigurationBuilderHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Accountant.SolutionBuilder
+{
+    public static class ConfigurationBuilderHelper
+    {
+        public static IConfigurationBuilder AddSolutionConfiguration(this IConfigurationBuilder configurationBuilder, IHostEnvironment hostEnvironment)
+        {
+            configurationBuilder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);            
+
+            return configurationBuilder;
+        }
+    }
+}
