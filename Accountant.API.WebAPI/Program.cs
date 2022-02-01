a9f2c493020a8d40e66a9f973e27db46ebb07d84
Huw Parry <huparry@deloitte.co.uk>
2022-02-01 15:37:49 +0000

    Added X-API-Key authentication (Middleware and Wagger configuration).

--- Accountant.API.WebAPI/Program.cs
+++ Accountant.API.WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Accountant.API.WebAPI.Middleware;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("ApiKeyAuth", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "X-API-Key",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "ApiKey",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "X-API-Key header required."
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "ApiKeyAuth"
+                    }
+                },
+                new string[] {}
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -19,6 +47,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<ApiKeyAuthenticationMiddleware>();
 
 app.MapControllers();
 
