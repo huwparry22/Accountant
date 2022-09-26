429d011d81455fac63caf2e0cdbc24482b0ae7eb
Huw Parry <huwparry22@hotmail.com>
2022-09-26 19:58:04 +0100

    Added project services helper class.

---
+++ Accountant.API/Bootstrapper/ProjectServiceCollectionHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accountant.API.Bootstrapper
+{
+    public static class ProjectServiceCollectionHelper
+    {
+        public static void AddApiProjectServices(this IServiceCollection services)
+        {
+
+        }
+    }
+}
