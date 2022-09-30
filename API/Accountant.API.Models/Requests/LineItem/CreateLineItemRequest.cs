99a5156e01cb59e3cd08f55969fed4c7892ce9fa
Huw Parry <huparry@deloitte.co.uk>
2022-09-30 12:20:00 +0100

    Remove warning and make Description property a nullable string.

--- API/Accountant.API.Models/Requests/LineItem/CreateLineItemRequest.cs
+++ API/Accountant.API.Models/Requests/LineItem/CreateLineItemRequest.cs
@@ -10,6 +10,6 @@ namespace Accountant.API.Models.Requests.LineItem
     public class CreateLineItemRequest : BaseRequest
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "Description required")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
     }
 }
