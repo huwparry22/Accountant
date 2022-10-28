71ada6e12dbab9a004b29c1595a454f3e747d83a
Huw Parry <huwparry22@hotmail.com>
2022-10-28 16:14:02 +0100

    Added implementation to get ApiProcess basedon request and response object types.

--- API/Accountant.API/Processes/ApiProcessFactory.cs
+++ API/Accountant.API/Processes/ApiProcessFactory.cs
@@ -1,13 +1,22 @@
 ﻿using Accountant.API.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Accountant.API.Models;
 
 namespace Accountant.API.Processes
 {
     public class ApiProcessFactory : IApiProcessFactory
     {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ApiProcessFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IApiProcess<TRequest, TResponse> GetApiProcess<TRequest, TResponse>()
+            where TRequest : BaseRequest
+            where TResponse : BaseResponse
+        {
+            return _serviceProvider.GetService(typeof(IApiProcess<TRequest, TResponse>)) as IApiProcess<TRequest, TResponse>;
+        }
     }
 }
