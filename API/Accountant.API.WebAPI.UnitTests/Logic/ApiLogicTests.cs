1c6c067e327f8c20b5160885c121958732f3f30d
Huw Parry <huwparry22@hotmail.com>
2022-10-29 15:37:44 +0100

    Started adding scaffolding for RunApiProcess tests.

--- API/Accountant.API.WebAPI.UnitTests/Logic/ApiLogicTests.cs
+++ API/Accountant.API.WebAPI.UnitTests/Logic/ApiLogicTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Accountant.API.Models.Requests.LineItem;
+using Accountant.API.Models.Responses.LineItem;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace Accountant.API.WebAPI.UnitTests.Logic
 {
-    internal class ApiLogicTests
+    public class ApiLogicTests
     {
+        private readonly ApiLogic _objectToTest;
+
+        private readonly Mock<IApiProcessFactory> _mockApiProcessFactory;
+
+        public ApiLogicTests()
+        {
+            _mockApiProcessFactory = new Mock<IApiProcessFactory>();
+
+            _objectToTest = new ApiLogic(_mockApiProcessFactory.Object);
+        }
+
+        public class RunApiProcessTests : ApiLogicTests
+        {
+            private readonly Mock<IApiProcess<CreateLineItemRequest, CreateLineItemResponse>> _mockCreateLiineItemApiProcess;
+
+            public RunApiProcessTests() : base()
+            {
+                _mockCreateLiineItemApiProcess = new Mock<IApiProcess<CreateLineItemRequest, CreateLineItemResponse>>();
+
+                _mockApiProcessFactory
+                    .Setup(x => x.GetApiProcess<CreateLineItemRequest, CreateLineItemResponse>())
+                    .Returns(_mockCreateLiineItemApiProcess.Object);
+            }
+        }
     }
 }
--- API/Accountant.API.WebAPI.UnitTests/Usings.cs
+++ API/Accountant.API.WebAPI.UnitTests/Usings.cs
@@ -1,2 +1,4 @@
 global using Xunit;
-global using Accountant.API.WebAPI;
+global using Accountant.API.WebAPI.Logic;
+global using Accountant.API.Interfaces;
+global using Moq;
