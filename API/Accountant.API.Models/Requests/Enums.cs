56fc20430c51e6da9c0429df0dd5a4db8917961e
Huw Parry <huparry@deloitte.co.uk>
2022-12-14 11:12:40 +0000

    Added create sub line item request. Added enum to support sub line item type.

---
+++ API/Accountant.API.Models/Requests/Enums.cs
@@ -0,0 +1,8 @@
+﻿namespace Accountant.API.Models.Requests
+{
+    public enum SubLineItemType
+    {
+        Income = 1,
+        Expenditure = 2
+    }
+}
---
+++ API/Accountant.API.Models/Requests/SubLineItem/CreateSubLineItemRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Accountant.API.Models.Requests.SubLineItem
+{
+    public  class CreateSubLineItemRequest : BaseRequest
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "LineItemId required")]
+        public int? LineItemId { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "SubLineItemType required")]
+        public SubLineItemType? SubLineItemType { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Description required")]
+        public string? Description { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Amount required")]
+        public decimal? Amount { get; set; }
+    }
+}
