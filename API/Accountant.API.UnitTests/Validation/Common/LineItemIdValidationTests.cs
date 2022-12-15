7afabb967b1e8dbd2d653f9bc77f9d2dd9a8d72f
Huw Parry <huparry@deloitte.co.uk>
2022-12-15 15:58:12 +0000

    Started adding LineItemIdValidationTests.

---
+++ API/Accountant.API.UnitTests/Validation/Common/LineItemIdValidationTests.cs
@@ -0,0 +1,25 @@
+﻿using Accountant.API.Validation.Common;
+using Accountant.Core.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accountant.API.UnitTests.Validation.Common
+{
+    public class LineItemIdValidationTests
+    {
+        private readonly Mock<ILineItemLogic> _mockLineItemLogic;
+
+        private readonly LineItemIdValidation _objectToTest;
+
+        public LineItemIdValidationTests()
+        {
+            _mockLineItemLogic = new Mock<ILineItemLogic>();
+            
+            _objectToTest = new LineItemIdValidation(_mockLineItemLogic.Object);
+        }
+    }
+}
