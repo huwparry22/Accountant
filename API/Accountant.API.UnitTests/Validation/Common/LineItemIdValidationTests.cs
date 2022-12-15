2f03c40c2c6a6cee7f8f07f7d1e570337119f30e
Huw Parry <huparry@deloitte.co.uk>
2022-12-15 16:09:24 +0000

    Added valid test case scenario.

--- API/Accountant.API.UnitTests/Validation/Common/LineItemIdValidationTests.cs
+++ API/Accountant.API.UnitTests/Validation/Common/LineItemIdValidationTests.cs
@@ -1,5 +1,8 @@
-﻿using Accountant.API.Validation.Common;
+﻿using Accountant.API.Models.Requests.SubLineItem;
+using Accountant.API.Validation.Common;
 using Accountant.Core.Interfaces;
+using Accountant.Data.Entities;
+using FluentValidation.TestHelper;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -18,8 +21,32 @@ namespace Accountant.API.UnitTests.Validation.Common
         public LineItemIdValidationTests()
         {
             _mockLineItemLogic = new Mock<ILineItemLogic>();
+
             
             _objectToTest = new LineItemIdValidation(_mockLineItemLogic.Object);
+        }
+
+        [Fact]
+        public async Task ValidLineItemId()
+        {
+            var lineItem = new Data.Entities.LineItem
+            {
+                LineItemId = 99
+            };
+
+            _mockLineItemLogic
+                .Setup(x => x.GetLineItemByLineItemId(It.IsAny<int>()))
+                .ReturnsAsync(lineItem);
+
+            var request = new CreateSubLineItemRequest
+            {
+                LineItemId = lineItem.LineItemId
+            };
+
+            var actual = await _objectToTest.TestValidateAsync(request).ConfigureAwait(false);
+
+            _mockLineItemLogic.Verify(x => x.GetLineItemByLineItemId(request.LineItemId.Value), Times.Once());
+            actual.ShouldNotHaveAnyValidationErrors();
         }
     }
 }
