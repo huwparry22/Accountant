2e1eeca6d0d5f1b09db14153344d72de90a6fc9e
Huw Parry <huparry@deloitte.co.uk>
2022-12-15 14:53:51 +0000

    Added implementation to sub line item logic, create sub line item.

--- API/Accountant.Core/Interfaces/ISubLineItemMapper.cs
+++ API/Accountant.Core/Interfaces/ISubLineItemMapper.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Accountant.API.Models.Requests.SubLineItem;
+using Accountant.Data.Entities;
 
 namespace Accountant.Core.Interfaces
 {
     public interface ISubLineItemMapper
     {
+        SubLineItem GetSubLineItem(CreateSubLineItemRequest subLineItemRequest);
     }
 }
--- API/Accountant.Core/Logic/SubLineItemLogic.cs
+++ API/Accountant.Core/Logic/SubLineItemLogic.cs
@@ -1,13 +1,27 @@
-﻿using Accountant.Core.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Accountant.API.Models.Requests.SubLineItem;
+using Accountant.Core.Interfaces;
+using Accountant.Data.EntityProviders;
 
 namespace Accountant.Core.Logic
 {
     public class SubLineItemLogic : ISubLineItemLogic
     {
+        private readonly ISubLineItemMapper _subLineItemMapper;
+        private readonly ISubLineItemProvider _subLineItemProvider;
+
+        public SubLineItemLogic(ISubLineItemMapper subLineItemMapper, ISubLineItemProvider subLineItemProvider)
+        {
+            _subLineItemMapper = subLineItemMapper;
+            _subLineItemProvider = subLineItemProvider;
+        }
+
+        public async Task<int> CreateSubLineItem(CreateSubLineItemRequest createSubLineItemRequest)
+        {
+            var entity = _subLineItemMapper.GetSubLineItem(createSubLineItemRequest);
+
+            await _subLineItemProvider.SaveAsync(entity).ConfigureAwait(false);
+
+            return entity.SubLineItemId;
+        }
     }
 }
