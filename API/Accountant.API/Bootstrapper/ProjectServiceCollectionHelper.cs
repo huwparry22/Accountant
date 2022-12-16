cba141fc8cee71a41546fd8823e2bb98107a1b68
Huw Parry <huparry@deloitte.co.uk>
2022-12-16 11:02:08 +0000

    Added CreateSubLineItemValidation service registration. Started adding unit tests.

--- API/Accountant.API/Bootstrapper/ProjectServiceCollectionHelper.cs
+++ API/Accountant.API/Bootstrapper/ProjectServiceCollectionHelper.cs
@@ -2,11 +2,13 @@
 using Accountant.API.Mappers;
 using Accountant.API.Models.Interfaces;
 using Accountant.API.Models.Requests.LineItem;
+using Accountant.API.Models.Requests.SubLineItem;
 using Accountant.API.Models.Responses.LineItem;
 using Accountant.API.Processes;
 using Accountant.API.Processes.LineItem;
 using Accountant.API.Validation.Common;
 using Accountant.API.Validation.LineItem;
+using Accountant.API.Validation.SubLineItem;
 using FluentValidation;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -21,6 +23,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
             services.AddTransient<IValidator<ILineItemId>, LineItemIdValidation>();
             services.AddTransient<IValidator<CreateLineItemRequest>, CreateLineItemValidation>();
+            services.AddTransient<IValidator<CreateSubLineItemRequest>, CreateSubLineItemValidation>();
             
 
             services.AddTransient<IValidationResultMapper, ValidationResultMapper>();
