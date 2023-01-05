2aa2682bb76e8849f61bb006d482a3977e7bb378
Huw Parry <huparry@deloitte.co.uk>
2023-01-05 12:07:52 +0000

    Added Validate unit tests.

--- API/Accountant.API.UnitTests/Processes/SubLineItem/CreateSubLineItemProcessTests.cs
+++ API/Accountant.API.UnitTests/Processes/SubLineItem/CreateSubLineItemProcessTests.cs
@@ -9,6 +9,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Moq;
 using Accountant.API.Processes.SubLineItem;
+using Accountant.API.Models.Requests.LineItem;
+using Accountant.API.Models.Responses.LineItem;
+using Accountant.API.Models.Responses.SubLineItem;
+using FluentValidation.Results;
 
 namespace Accountant.API.UnitTests.Processes.SubLineItem
 {
@@ -27,6 +31,57 @@ namespace Accountant.API.UnitTests.Processes.SubLineItem
             _mockSubLineItemLogic = new Mock<ISubLineItemLogic>();
 
             _objectToTest = new CreateSubLineItemProcess(_mockValidator.Object, _mockValidationResultMapper.Object, _mockSubLineItemLogic.Object);
+        }
+
+        public class ValidateTests : CreateSubLineItemProcessTests
+        {
+            private readonly CreateSubLineItemRequest _request;
+
+            private readonly ValidationResult _validationResult;
+
+            private readonly CreateSubLineItemResponse _createSubLineItemResponse;
+
+            public ValidateTests() : base()
+            {
+                _request = new CreateSubLineItemRequest();
+
+                _validationResult = new ValidationResult();
+
+                _createSubLineItemResponse = new CreateSubLineItemResponse();
+
+                _mockValidator
+                    .Setup(x => x.ValidateAsync(It.IsAny<CreateSubLineItemRequest>(), It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(_validationResult);
+
+                _mockValidationResultMapper
+                    .Setup(x => x.MapToApiResponse<CreateSubLineItemResponse>(It.IsAny<ValidationResult>()))
+                    .Returns(_createSubLineItemResponse);
+            }
+
+            [Fact]
+            public async Task CallsValidatorValidateAsync()
+            {
+                var result = await _objectToTest.Validate(_request).ConfigureAwait(false);
+
+                _mockValidator.Verify(x => x.ValidateAsync(_request, It.IsAny<CancellationToken>()), Times.Once());
+            }
+
+            [Fact]
+            public async Task CallsValidationResultMapperMapToApiResponse()
+            {
+                var result = await _objectToTest.Validate(_request).ConfigureAwait(false);
+
+                _mockValidationResultMapper.Verify(x => x.MapToApiResponse<CreateSubLineItemResponse>(_validationResult),
+                    Times.Once());
+            }
+
+            [Fact]
+            public async Task ReturnsCreateLineItemResponse()
+            {
+                var result = await _objectToTest.Validate(_request).ConfigureAwait(false);
+
+                result.Should().Be(_createSubLineItemResponse);
+            }
         }
     }
 }
