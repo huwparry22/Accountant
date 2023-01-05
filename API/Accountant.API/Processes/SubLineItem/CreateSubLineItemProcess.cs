ea8141dd6a1e1d671d84c98d4c5da42beb6f1862
Huw Parry <huparry@deloitte.co.uk>
2023-01-05 11:47:35 +0000

    Added constructor implementation.

--- API/Accountant.API/Processes/SubLineItem/CreateSubLineItemProcess.cs
+++ API/Accountant.API/Processes/SubLineItem/CreateSubLineItemProcess.cs
@@ -6,11 +6,24 @@ using System.Text;
 using System.Threading.Tasks;
 using Accountant.API.Models.Requests.SubLineItem;
 using Accountant.API.Models.Responses.SubLineItem;
+using FluentValidation;
+using Accountant.Core.Interfaces;
 
 namespace Accountant.API.Processes.SubLineItem
 {
     public class CreateSubLineItemProcess : IApiProcess<CreateSubLineItemRequest, CreateSubLineItemResponse>
     {
+        private readonly IValidator<CreateSubLineItemRequest> _validator;
+        private readonly IValidationResultMapper _validationResultMapper;
+        private readonly ISubLineItemLogic _subLineItemLogic;
+
+        public CreateSubLineItemProcess(IValidator<CreateSubLineItemRequest> validator, IValidationResultMapper validationResultMapper, ISubLineItemLogic subLineItemLogic)
+        {
+            _validator = validator;
+            _validationResultMapper = validationResultMapper;
+            _subLineItemLogic = subLineItemLogic;
+        }
+
         public Task<CreateSubLineItemResponse> Validate(CreateSubLineItemRequest request)
         {
             throw new NotImplementedException();
