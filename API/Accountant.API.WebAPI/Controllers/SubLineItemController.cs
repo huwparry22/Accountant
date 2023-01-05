3300ccb42ab63a35927e45b2988447cca356b2ca
Huw Parry <huparry@deloitte.co.uk>
2023-01-05 12:13:27 +0000

    Added controller for sub line item. Added create sub lline item method.

---
+++ API/Accountant.API.WebAPI/Controllers/SubLineItemController.cs
@@ -0,0 +1,25 @@
+﻿using Accountant.API.Models.Requests.SubLineItem;
+using Accountant.API.Models.Responses.SubLineItem;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accountant.API.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubLineItemController : ControllerBase
+    {
+        private readonly IApiLogic _apiLogic;
+
+        public SubLineItemController(IApiLogic apiLogic)
+        {
+            _apiLogic = apiLogic;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<ActionResult<CreateSubLineItemResponse>> Create(CreateSubLineItemRequest createSubLineItemRequest)
+        {
+            return await _apiLogic.RunApiProcess<CreateSubLineItemRequest, CreateSubLineItemResponse>(createSubLineItemRequest).ConfigureAwait(false);
+        }
+    }
+}
