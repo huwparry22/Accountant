705435a37988a98475adbf9076e72f3e67b9622d
Huw Parry <huwparry22@hotmail.com>
2023-03-02 18:00:32 +0000

    Removed no longer used User entity properties. Added ef core migration.

--- API/Accountant.Data.Entities/User.cs
+++ API/Accountant.Data.Entities/User.cs
@@ -4,15 +4,9 @@
     {
         public int UserId { get; set; }
 
-        public int UserName { get; set; }
-
         public string EmailAddress { get; set; }
 
-        public string Title { get; set; }
-
         public string FirstName { get; set; }
-
-        public string MiddleName { get; set; }
 
         public string LastName { get; set; }
     }
--- API/Accountant.Data.SqlServer.Migrations/Migrations/20230302175919_UserEntityUpdate.Designer.cs
+++ API/Accountant.Data.SqlServer.Migrations/Migrations/20230302175919_UserEntityUpdate.Designer.cs
@@ -4,6 +4,7 @@ using Accountant.Data.SqlServer.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Accountant.Data.SqlServer.Migrations.Migrations
 {
     [DbContext(typeof(AccountantContext))]
-    partial class AccountantContextModelSnapshot : ModelSnapshot
+    [Migration("20230302175919_UserEntityUpdate")]
+    partial class UserEntityUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
