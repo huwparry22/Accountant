327c6aab3f68a6764063118ac87ec1b71028c45c
Huw Parry <huwparry22@hotmail.com>
2023-03-02 15:21:08 +0000

    Added user property to base request.

--- API/Accountant.API.Models/BaseRequest.cs
+++ API/Accountant.API.Models/BaseRequest.cs
@@ -1,6 +1,10 @@
-﻿namespace Accountant.API.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Accountant.API.Models
 {
     public class BaseRequest
     {
+        [JsonIgnore]
+        public User AuthenticatedUser { get; set; }
     }
 }
