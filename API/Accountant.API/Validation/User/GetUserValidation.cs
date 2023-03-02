3c07b1e30fadbf40db2ad6845bbee382a3f12cb1
Huw Parry <huwparry22@hotmail.com>
2023-03-02 18:23:07 +0000

    Started adding email address validation.

---
+++ API/Accountant.API/Validation/User/GetUserValidation.cs
@@ -0,0 +1,65 @@
+﻿using Accountant.API.Models.Requests.LineItem;
+using Accountant.API.Models.Requests.User;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Accountant.API.Validation.User
+{
+    public class GetUserValidation : AbstractValidator<GetUserRequest>
+    {
+        public GetUserValidation()
+        {
+            RuleFor(getUserRequest => getUserRequest.EmailAddress)
+                .NotEmpty()
+                .Must(ValidEmailAddress)
+                .WithMessage("Invalid email address");
+        }
+
+        private static bool ValidEmailAddress(string? emailAddress)
+        {
+            try
+            {
+                // Normalize the domain
+                emailAddress = Regex.Replace(emailAddress, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(emailAddress,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+    }
+}
