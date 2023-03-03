e311025a91a8879b07bd34ebc44335d847744f4d
Huw Parry <huwparry22@hotmail.com>
2023-03-03 10:57:37 +0000

    Removed commonn email address validation. Stick to simple fluentvalidation and model attributiion on request.

--- API/Accountant.API.Models/Requests/User/GetUserRequest.cs
+++ API/Accountant.API.Models/Requests/User/GetUserRequest.cs
@@ -5,6 +5,7 @@ namespace Accountant.API.Models.Requests.User
     public class GetUserRequest : BaseRequest
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "Email address required")]
+        [EmailAddress]
         public string? EmailAddress { get; set; }
     }
 }
--- API/Accountant.API/Validation/User/GetUserValidation.cs
+++ API/Accountant.API/Validation/User/GetUserValidation.cs
@@ -17,49 +17,8 @@ namespace Accountant.API.Validation.User
         {
             RuleFor(getUserRequest => getUserRequest.EmailAddress)
                 .NotEmpty()
-                .Must(ValidEmailAddress)
+                .EmailAddress()
                 .WithMessage("Invalid email address");
-        }
-
-        private static bool ValidEmailAddress(string? emailAddress)
-        {
-            try
-            {
-                // Normalize the domain
-                emailAddress = Regex.Replace(emailAddress, @"(@)(.+)$", DomainMapper,
-                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
-
-                // Examines the domain part of the email and normalizes it.
-                string DomainMapper(Match match)
-                {
-                    // Use IdnMapping class to convert Unicode domain names.
-                    var idn = new IdnMapping();
-
-                    // Pull out and process domain name (throws ArgumentException on invalid)
-                    string domainName = idn.GetAscii(match.Groups[2].Value);
-
-                    return match.Groups[1].Value + domainName;
-                }
-            }
-            catch (RegexMatchTimeoutException e)
-            {
-                return false;
-            }
-            catch (ArgumentException e)
-            {
-                return false;
-            }
-
-            try
-            {
-                return Regex.IsMatch(emailAddress,
-                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
-                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
-            }
-            catch (RegexMatchTimeoutException)
-            {
-                return false;
-            }
-        }
+        }        
     }
 }
