1534e8c34c2a85f7851d6d9d4d5581de425d2ed5
Huw Parry <huwparry22@hotmail.com>
2023-03-07 19:49:06 +0000

    started adding validation test class for get user process.

--- API/Accountant.API.UnitTests/Processes/User/GetUserProcessTests.cs
+++ API/Accountant.API.UnitTests/Processes/User/GetUserProcessTests.cs
@@ -1,7 +1,9 @@
 ﻿using Accountant.API.Interfaces;
 using Accountant.API.Models.Requests.User;
+using Accountant.API.Models.Responses.User;
 using Accountant.API.Processes.User;
 using FluentValidation;
+using FluentValidation.Results;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -24,6 +26,29 @@ namespace Accountant.API.UnitTests.Processes.User
             _mockValidationResultMapper = new Mock<IValidationResultMapper>();
 
             _objectToTest = new GetUserProcess(_mockGetUserValidation.Object, _mockValidationResultMapper.Object);
+        }
+
+        public class ValidateTests : GetUserProcessTests
+        {
+            private readonly GetUserRequest _request;
+            private readonly ValidationResult _validationResult;
+            private readonly GetUserResponse _response;
+
+
+            public ValidateTests() : base()
+            {
+                _request = new GetUserRequest();
+                _validationResult = new ValidationResult();
+                _response = new GetUserResponse();
+
+                _mockGetUserValidation
+                    .Setup(x => x.ValidateAsync(It.IsAny<GetUserRequest>(), It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(_validationResult);
+
+                _mockValidationResultMapper
+                    .Setup(x => x.MapToApiResponse<GetUserResponse>(It.IsAny<ValidationResult>()))
+                    .Returns(_response);
+            }
         }
     }
 }
