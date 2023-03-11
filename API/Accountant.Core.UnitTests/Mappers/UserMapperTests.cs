7cae2dbaeec3d8ce6f20c6059e32a19d1acc9332
Huw Parry <huwparry22@hotmail.com>
2023-03-11 14:44:06 +0000

    Added unit test for user mapper method.

---
+++ API/Accountant.Core.UnitTests/Mappers/UserMapperTests.cs
@@ -0,0 +1,56 @@
+﻿using Accountant.Core.Mappers;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Accountant.Core.UnitTests.Mappers
+{
+    public class UserMapperTests
+    {
+        private readonly UserMapper _objectToTest;
+
+        public UserMapperTests()
+        {
+            _objectToTest = new UserMapper();
+        }
+
+        [Theory]
+        [ClassData(typeof(UserMapperMapToModelUserTestData))]
+        public void MapToModelUser(Data.Entities.User parameter, API.Models.User expected)
+        {
+            if (parameter == null)
+            {
+                _objectToTest.Invoking(o => o.MapToModelUser(parameter))
+                    .Should()
+                    .ThrowExactly<ArgumentNullException>();
+            }
+            else
+            {
+                var actual = _objectToTest.MapToModelUser(parameter);
+
+                actual.Should().BeEquivalentTo(expected);
+            }
+        }
+
+
+        public class UserMapperMapToModelUserTestData : TheoryData<Data.Entities.User, API.Models.User>
+        {
+            public UserMapperMapToModelUserTestData()
+            {
+                Add(null, null);
+
+                Add(
+                    new Data.Entities.User
+                    {
+                        UserId = 99,
+                        EmailAddress = "test@test.com"
+                    },
+                    new API.Models.User
+                    {
+                        UserId = 99,
+                        EmailAddress = "test@test.com"
+                    });
+            }
+        }
+    }
+}
